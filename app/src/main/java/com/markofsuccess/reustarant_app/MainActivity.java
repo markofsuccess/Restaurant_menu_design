dd149e5f7a9e13ed4a262b1089f9c9ebe4f01648
macbook <markussjoman1@gmail.com>
2023-03-28 10:40:10 +0200

    added intent for startersactivity in the MainActivity class so activity screen can be opened.

--- app/src/main/java/com/markofsuccess/reustarant_app/MainActivity.java
+++ app/src/main/java/com/markofsuccess/reustarant_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.markofsuccess.reustarant_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -21,8 +22,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-   CardView startersCard;
-   CardView mainsCard;
+    CardView startersCard;
+    CardView mainsCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +35,21 @@ public class MainActivity extends AppCompatActivity {
         startersCard.findViewById(R.id.card_view_starters);
         mainsCard.findViewById(R.id.card_view_main);
 
+        startersCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                //MainActivity.this means where we are right now and after the comma is where we want to go -->StartersActivity class
+                Intent startersActivityIntent = new Intent(MainActivity.this,StartersActivity.class);
+
+                //put the intent in the startActivity method
+                // what this will do is that it will read the intent and say oh you want to go the startersActivity
+                // and it will read the manifest and it will find the the starters activity and open the screen.
+                //intent can be used for moving between screens, how to move to a new activity
+                startActivity(startersActivityIntent);
             }
-
+        });
     }
+
+}
 
