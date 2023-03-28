ce73fcd5c6b2921a8d4fe0743650c65d1e3f432c
macbook <markussjoman1@gmail.com>
2023-03-28 10:15:35 +0200

    added StartersActivity class and layout

---
+++ app/src/main/java/com/markofsuccess/reustarant_app/StartersActivity.java
@@ -0,0 +1,14 @@
+package com.markofsuccess.reustarant_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starters);
+    }
+}
